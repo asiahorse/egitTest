d37d5f165897e4f84bef3608635b6502d261e1d5
asiahorse666 <2535786279@qq.com>
2020-03-20 20:07:37 +0800

    添加7777

--- src/com/atguigu/egit/TestEgit.java
+++ src/com/atguigu/egit/TestEgit.java
@@ -4,5 +4,6 @@ public class TestEgit {
 	public static void main(String[] args) {
 		System.out.println("hello egit");
 		System.out.println("hello egit6666666");
+		System.out.println("hello egit7777");
 	}
 }
