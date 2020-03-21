7e725ad0275afecc25f6f1df0fc5ed6798829faa
asiahorse666 <2535786279@qq.com>
2020-03-21 09:36:54 +0800

    创建了一个新的分支--666

--- src/com/atguigu/egit/TestEgit.java
+++ src/com/atguigu/egit/TestEgit.java
@@ -6,5 +6,6 @@ public class TestEgit {
 		System.out.println("hello egit6666666");
 		System.out.println("hello egit88888");
 		System.out.println("hello egit7777");
+		System.out.println("hello 创建了一个新的分支--666");
 	}
 }
